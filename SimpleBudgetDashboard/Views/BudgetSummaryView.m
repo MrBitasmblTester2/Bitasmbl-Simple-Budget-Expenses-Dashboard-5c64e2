cda797b519a1f47ad1150cb55765f60f5b2c8c0f
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 12:49:39 +0400

    Add BudgetSummaryView.m

---
+++ SimpleBudgetDashboard/Views/BudgetSummaryView.m
@@ -0,0 +1,10 @@
+#import "BudgetSummaryView.h"
+@implementation BudgetSummaryView
+- (instancetype)initWithFrame:(CGRect)frame{
+ if(self=[super initWithFrame:frame]){
+  self.backgroundColor=[UIColor systemGray6Color];
+ }
+ return self;
+}
+- (void)configure{}
+@end
