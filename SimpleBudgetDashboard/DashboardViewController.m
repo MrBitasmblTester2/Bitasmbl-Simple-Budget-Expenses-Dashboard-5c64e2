292a4c78911199e411ea3d835a9d77cfecbcab90
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 12:49:35 +0400

    Add DashboardViewController.m

---
+++ SimpleBudgetDashboard/DashboardViewController.m
@@ -0,0 +1,8 @@
+#import "DashboardViewController.h"
+@implementation DashboardViewController
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.view.backgroundColor=[UIColor whiteColor];
+  self.title=@"Budget";
+}
+@end
