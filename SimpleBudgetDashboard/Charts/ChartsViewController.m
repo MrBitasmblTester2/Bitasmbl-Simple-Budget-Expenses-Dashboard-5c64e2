28ed10cc9b2147118fef18d49a2c56317f9263bb
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 12:49:45 +0400

    Add ChartsViewController.m

---
+++ SimpleBudgetDashboard/Charts/ChartsViewController.m
@@ -0,0 +1,8 @@
+#import "ChartsViewController.h"
+@implementation ChartsViewController
+- (void)viewDidLoad{
+ [super viewDidLoad];
+ self.view.backgroundColor=[UIColor whiteColor];
+ self.title=@"Charts";
+}
+@end
