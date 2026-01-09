bccc09d4ecc0cb954e213a1b40f9bc4289f9feed
MrBitasmblTester2 <lulu2214@proton.me>
2026-01-09 12:49:33 +0400

    Add AppDelegate.m

---
+++ SimpleBudgetDashboard/AppDelegate.m
@@ -0,0 +1,10 @@
+#import "AppDelegate.h"
+#import "DashboardViewController.h"
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  self.window=[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+  self.window.rootViewController=[DashboardViewController new];
+  [self.window makeKeyAndVisible];
+  return YES;
+}
+@end
